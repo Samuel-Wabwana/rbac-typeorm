f0e89e5505bbbecb6b20fa9e734e83d190cef586
Samuel Wabwana <samuelwabwana@gmail>
2023-03-14 08:25:21 -0700

    implementation of create user functionnality

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -12,7 +14,11 @@ export class UsersService {
     async findAll() {
         return await this.userRepository.find();
     }
-    async create(createUserDto) {
-        return await this.userRepository.save(createUserDto);
+    async create(createUserDto: CreateUserDto) {
+        const saltOrRounds = 10;
+        const salt = await bcrypt.genSalt(saltOrRounds);
+        createUserDto.password = await bcrypt.hash(createUserDto.password, salt);
+        const { password, ...retrieveUserDto } = await this.userRepository.save(createUserDto);
+        return retrieveUserDto;
     }
 }
